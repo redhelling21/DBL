85ab29e2ea60e0abde571f26a404c68dd0dde77c
Théo Buri <theo.buri@gmail.com>
2018-03-22 14:01:38 +0100

    Small bar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  screenSizeClass: String = 'bar-large';
 
-  constructor() { }
+  constructor(private zone: NgZone) {
+    let mq = window.matchMedia('(max-width:767px)');
+    if (mq.matches) {
+      this.screenSizeClass = 'bar-small';
+    }
+    matchMedia('(max-width:767px)').addListener((mql => {
+      if (mql.matches) {
+        this.zone.run(() => {
+          this.screenSizeClass = 'bar-small';
+        });
+      }
+    }));
+    matchMedia('(min-width:768px)').addListener((mql => {
+      if (mql.matches) {
+        this.zone.run(() => {
+          this.screenSizeClass = 'bar-large';
+        });
+      }
+    }));
+  }
 
   ngOnInit() {
+
+  }
+
+  toggleSizeBar() {
+    if(this.screenSizeClass === 'bar-large'){
+      this.screenSizeClass = 'bar-small';
+    } else {
+      this.screenSizeClass = 'bar-large';
+    }
   }
 
 }
